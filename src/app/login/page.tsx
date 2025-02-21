00929098fac3372d83cb05401ba4b9ab352e2aef
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-02-21 19:32:55 +0530

    dyn imp

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
-"use client";
-import AuthForm from "./auth-form";
+"use client"; // This ensures it's treated as a client component
 
+import dynamic from "next/dynamic";
+const AuthForm = dynamic(() => import("./auth-form"), { ssr: false });
 
 export default function LoginPage() {
   return <AuthForm />;
